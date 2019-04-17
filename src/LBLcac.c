356de672f154b8927fb91ce5056193041d4c7b78
mxw010 <cherrywang1006@gmail.com>
2019-04-17 15:47:20 -0400

    debugged warning messages while compiling

--- src/LBLcac.c
+++ src/LBLcac.c
@@ -360,7 +360,7 @@ void update_freq_cc(double *freq, double *beta, double D, int x_length, int N, i
     error("LBL ran into an error and has stopped\n");
   }
 
-  if (-min_f_new/(1-min_f_new)  > D & accept_prob>0) {
+  if ((-min_f_new/(1-min_f_new)  > D) & (accept_prob>0)) {
     Rprintf("Iteration %d error: in updating f: Min f_new and D constraint violated min_f_new=%f", n_iter, min_f_new);
     Rprintf("current frequncy=(");
     for (i=0; i<x_length; ++i)
